efa7d405250abd7356e5929d0f8a392076773620
Gabriel Patara <gabriel.patara@gmail.com>
2024-07-29 13:47:42 -0300

    primeiro commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component:AppComponent,children:[
+        {path:'',component:HomeComponent}
+    ]},
+];
